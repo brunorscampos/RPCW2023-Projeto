18d0680ddb03f9af47666142d57dd02b7a04af23
jtmr05 <62111562+jtmr05@users.noreply.github.com>
2023-05-20 02:52:07 +0100

    parsing documents correctly (untested)

--- clean_up/src/main.cpp
+++ clean_up/src/main.cpp
@@ -1,5 +1,6 @@
 #include "json.hpp"
 
+#include <cstdio>
 #include <exception>
 #include <memory>
 #include <optional>
@@ -49,47 +50,49 @@ public:
 
             if(m_ifh.get() == nullptr)
                 throw std::ios::failure{"IO error at constructor for document_stream_t"};
+
+            this->read_chunk();
         }
 
     std::optional<nlohmann::json> next_document(){
 
         static std::string document_buffer {};
 
-READ:
-        if(!this->resize_buffer())
-            throw std::bad_alloc();
+        char* pos {nullptr};
+        do {
+            if((pos = std::strstr(m_buffer.get(), m_input_rec_sep.c_str())) != nullptr)
+                break;
+        }
+        while(this->read_chunk() > 0);
 
-        size_t const chars_read {std::fread(
-            m_buffer.get() + m_buffer_size,
-            sizeof(*m_buffer),
-            std::min(m_chunk_size, m_buffer_capacity - m_buffer_size),
-            m_ifh.get()
-        )};
-        m_buffer_size += chars_read;
+        char char_at_pos {};
+        if(pos != nullptr){
+            char_at_pos = pos[m_input_rec_sep.size()];
+            pos[m_input_rec_sep.size()] = '\0';
+        }
 
-        char* const pos {std::strstr(m_buffer.get(), m_input_rec_sep.c_str())};
-        if(pos == nullptr)
-            goto READ;
-
-
-        char const char_at_pos {pos[m_input_rec_sep.size()]};
-        pos[m_input_rec_sep.size()] = '\0';
-
-        regmatch_t match_obj[1];
-        if(regexec(m_doc_regex_obj.get(), m_buffer.get(), 1, match_obj, 0) != 0)
+        std::array<regmatch_t, 1> match_obj {};
+        if(
+            regexec(
+                m_doc_regex_obj.get(),
+                m_buffer.get(),
+                match_obj.size(),
+                match_obj.data(),
+                0
+            ) == REG_NOMATCH
+        )
             return std::nullopt;
 
-        pos[m_input_rec_sep.size()] = char_at_pos;
+        if(pos != nullptr)
+            pos[m_input_rec_sep.size()] = char_at_pos;
 
         char const char_at_rm_eo {m_buffer.get()[match_obj[0].rm_eo]};
         m_buffer.get()[match_obj[0].rm_eo] = '\0';
-
         document_buffer = m_buffer.get() + match_obj[0].rm_so;
-
         m_buffer.get()[match_obj[0].rm_eo] = char_at_rm_eo;
 
 
-        this->adjust_buffer(pos + m_input_rec_sep.size());
+        this->adjust_buffer(m_buffer.get() + match_obj[0].rm_eo);
         return std::make_optional(nlohmann::json::parse(document_buffer));
     }
 
@@ -101,6 +104,21 @@ private:
     size_t m_buffer_size;
     std::unique_ptr<char, c_mem_deleter_t> m_buffer;
     std::unique_ptr<regex_t, regex_t_deleter_t> m_doc_regex_obj;
+
+    size_t read_chunk(){
+        if(!this->resize_buffer())
+            throw std::bad_alloc();
+
+        size_t const chars_read {std::fread(
+            m_buffer.get() + m_buffer_size,
+            sizeof(*m_buffer),
+            std::min(m_chunk_size, m_buffer_capacity - m_buffer_size),
+            m_ifh.get()
+        )};
+        m_buffer_size += chars_read;
+
+        return chars_read;
+    }
 
     void adjust_buffer(char* src_pos) noexcept {
 
@@ -156,11 +174,12 @@ int main(){
     document_stream_t doc_stream {"    },", "../datasets/atco1_acordaos.json"};
     size_t doc_count {0};
     while(true){
+
         std::optional<nlohmann::json> const j {doc_stream.next_document()};
         if(j.has_value()){
             ++doc_count;
             if(j.value().find("Nº do Documento") != j.value().end())
-                std::cout << j.value().find("Nº do Documento").value() << '\n';
+                std::cout << j.value().find("Nº do Documento").value() << std::endl;
         }
         else
             break;
